360479941cea7a3aa77819f7710725a05e4e7dd7
Justice <Contact@alifaraji.ir>
2019-12-24 00:24:05 +0330

    CAM Edited.

--- Memory/CAM.vhd
+++ Memory/CAM.vhd
@@ -1,20 +1,32 @@
--- Simple generic RAM Model
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:52:12 12/23/2019 
+-- Design Name: 
+-- Module Name:    CAM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
 --
--- +-----------------------------+
--- |    Copyright 2008 DOULOS    |
--- |   designer :  JK            |
--- +-----------------------------+
-
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.Numeric_Std.all;
 
 entity CAM is
   port (
     clock   : in  std_logic;
-    rw      : in  std_logic;
+    rw      : in  std_logic; -- 1 means write and 0 means read.
     reset      : in  std_logic;
-    datain  : in  std_logic_vector(2 downto 0);
+    data  : in  std_logic_vector(2 downto 0);
     match  : out std_logic
   );
 end entity CAM;
@@ -26,24 +38,32 @@ architecture RTL of CAM is
    signal validBit : std_logic_vector(15 downto 0) := (others => '0');
 	
 begin
-	
-	RamProc: process(clock, reset) is
+	CamProc: process(clock, reset) is
 		begin
 			if reset = '0' then
-				validBit := (others => '0');
+				validBit <=(others => '0');
 				
 			elsif rising_edge(clock) then
-				if w = '0' then
-					for i in 0 to 15 loop
-						if vaidBit(i)= '1' and ram(i) = datain then
+				if rw = '0' then
+					L1: for i in 0 to 15 loop
+						if validBit(i)= '1' and ram(i) = data then
 							match <= '1';
+							exit L1;
+						elsif to_unsigned(i,4) = to_unsigned(15,4) then
+							match <= '0';
+							exit L1;
 						end if;
-					end loop;
+					end loop L1;
 				else
-					
+					L2: for i in 0 to 15 loop
+						if validBit(i)= '0' then
+							validBit(i) <= '1';
+							ram(i) <= data;
+							exit L2;
+						end if;
+						end loop L2;
 				end if;
-				
 		end if;
-	end process RamProc;
+	end process CamProc;
 				
 end architecture RTL;
